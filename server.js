e8f212a6bbb8823c416ce5bd970b53f258416460
Ratchasri Kidking <orc_hero@hotmail.com>
2024-09-20 16:27:19 +0700

    Add search data

--- server.js
+++ server.js
@@ -2,8 +2,93 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient();
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
+
+// Call Data
+
+// Call data from database (Prisma) by import PrismaClient 
+app.get('/book/list', async (req,res) => {
+    const data = await prisma.book.findMany();
+    // send data as json ({})
+    res.send({ data: data})
+
+}) 
+// Add Book In Prisma from Front 
+app.post('/book/create' , async (req,res) => {
+    const data = req.body 
+    const result = await prisma.book.create({data:data})
+    res.send({result:result})
+})
+// Add Book In Prisma DataBase (Add Data)
+app.post('/book/createManual', async (req,res) => {
+    const result = await prisma.book.create({
+
+        data:{
+            isbn:'1003',
+            name:'React',
+            price: 4300
+        }
+        
+    })
+    res.send({data:result})
+})
+
+
+app.put('/book/update/:id' , async (req,res) =>{
+    try {
+        await prisma.book.update({
+            data: {
+                isbn: "1023",
+                name: 'NextJs',
+                price: 3000
+            },
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+        res.send({message:"success"})
+    } catch (error) {
+        res.send(500).send({error:error})
+    }
+    
+})
+
+app.delete('/book/remove/:id' , async (req,res) => {
+    await prisma.book.delete({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
+    res.send({ message: "message"})
+})
+
+
+app.post('/book/search' , async(req,res)=>{
+    const keyword = req.body.keyword
+    const data = await prisma.book.findMany({
+        where:{
+            name:{
+                contains: keyword,
+            }
+        }
+    })
+    res.send({results: data})
+})
+
+
+
+
+
+
+// End Prisma
+
+
 
 
 
